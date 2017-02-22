4b96de115e4f615ad149f5b016c76404545fda6e
Altamarit <alex.tamarit.b@gmail.com>
2017-02-22 21:02:16 +0100

    Images added

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -1,7 +1,7 @@
 package com.example.android.miwok;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -15,7 +15,7 @@ public class ColorsActivity extends AppCompatActivity {
 
         //  Array with numbers
         ArrayList<Word> words = new ArrayList<Word>();
-        words.add(new Word("red", "weṭeṭṭi"));
+        words.add(new Word("red", "weṭeṭṭi",R,));
         words.add(new Word("green", "chokokki"));
         words.add(new Word("brown", "ṭakaakki"));
         words.add(new Word("gray", "ṭopoppi"));
@@ -33,5 +33,8 @@ public class ColorsActivity extends AppCompatActivity {
         ListView listView = (ListView) findViewById(R.id.list);
         //  Pass the array adapter into the ListView
         listView.setAdapter(itemsAdapter);
+
+        // set the color background
+//        listView.setBackgroundColor(getColor(R.color.category_colors));
     }
 }
--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -10,12 +10,22 @@ public class Word {
     // The two states for this class are the default translation and the Miwok one
     private String mDefaultTranslation;
     private String mMiwokTranslation;
+    private int mImageId; // path to the image
 
 
-    // contructor
+    // constructor withOUT image
     public Word(String defaultTranslation, String miwokTranslation) {
         mDefaultTranslation = defaultTranslation;
         mMiwokTranslation = miwokTranslation;
+    }
+    // constructor with image
+    public Word(String defaultTranslation, String miwokTranslation, int imageId) {
+        mImageId = imageId;
+        mDefaultTranslation = defaultTranslation;
+        mMiwokTranslation = miwokTranslation;
+    }
+    public int getImage(){
+        return  mImageId;
     }
     public String getDefaultTranslation(){
         return  mDefaultTranslation;
